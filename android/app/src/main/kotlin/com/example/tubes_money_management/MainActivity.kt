f53ddf3fa18acd8aabf00a2a1aa3610da9765320
Arya <nyuteaa@gmail.com>
2024-01-14 12:04:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tubes_money_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tubes_money_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:tubes_money_management/models/transaction_model/transaction_model.dart';
+import 'package:tubes_money_management/provider/provider_category.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'models/category_model/category_model.dart';
+import 'provider/provider_transaction.dart';
+import 'view/screens/basescreen/splash_screen.dart';
+
+Future<void> main(context) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // transparent status bar
+  ));
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(TransactionModelAdapter().typeId)) {
+    Hive.registerAdapter(TransactionModelAdapter());
+  }
+
+  runApp(const MyApp());
+  Provider.of<ProviderTransaction>(context, listen: false).refreshUI();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => Providerinstence(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => ProviderTransaction(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
